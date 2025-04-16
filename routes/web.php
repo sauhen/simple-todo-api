b850ba6cef3faab6105a3602259777773ba0e13d
sauhen <savgen.anton@gmail.com>
2025-04-16 10:24:45 +0200

    simple-todo-list

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', fn() => redirect()->route('todos.index'));
-
-Route::middleware('guest')->group(function () {
-    Route::get('/login', fn() => Inertia::render('Auth/Login'))->name('login');
-    Route::get('/register', fn() => Inertia::render('Auth/Register'))->name('register');
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-});
-
-Route::middleware('auth')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
-    Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
-    Route::get('/todos/{todo}', [TodoController::class, 'show'])->name('todos.show');
-    Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
-    Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
+Route::get('/', function () {
+    return view('welcome');
 });
